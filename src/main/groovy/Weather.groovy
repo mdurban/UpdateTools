287f752b4a43d9ebd30a49033e61217390b59708
mdurban <murban117@gmail.com>
2016-01-24 12:36:23 -0500

    Change mocks to use metaclassing so that we can avoid the class having any state
    squash

--- src/main/groovy/Weather.groovy
+++ src/main/groovy/Weather.groovy
@@ -2,21 +2,27 @@ import groovy.json.*;
 
 class Weather {
     
-    private static final OPEN_WEATHER_MAP_EXAMPLE_APPID = '2de143494c0b295cca9337e1e96b00e0'
-    private static JsonSlurper slurper = new JsonSlurper()
+    // TODO - pass in appId as a command line property
+    private static final OPEN_WEATHER_MAP_EXAMPLE_APPID = 'e09874c013ded6bf8330a86122ba49c9'
 
     static def getCurrentTemperature(city, state) {
+        JsonSlurper slurper = new JsonSlurper()
         String jsonOutput = getWeatherData 'weather', '${city},${state}'
+
         convertToFahrenheit slurper.parseText(jsonOutput).main.temp
     }
     
     static def getCurrentConditions(city, state) {
+         JsonSlurper slurper = new JsonSlurper()
          String jsonOutput = getWeatherData 'weather', '${city},${state}'
+
          slurper.parseText(jsonOutput).weather.description
     }
 
     static def getFiveDayForecast(city, state) {
+        JsonSlurper slurper = new JsonSlurper()
          String jsonOutput = getWeatherData 'forecast/daily', '${city},${state}'
+
          def returnList = []
          slurper.parseText(jsonOutput).list.each {
              returnList << it.temp.day
--- src/test/groovy/WeatherSpec.groovy
+++ src/test/groovy/WeatherSpec.groovy
@@ -7,21 +7,19 @@ class WeatherSpec extends Specification {
 
     def "getCurrentTemperature should return current temperature of city"() {
         setup:
-           def slurper = [parseText: {
+            JsonSlurper.metaClass.parseText = {
                String json -> ["main":["temp": KELVIN_TEMP]]
-           }] as JsonSlurper
-           Weather.slurper = slurper
-           
+            }
+        
         expect:
            Weather.getCurrentTemperature("columbus", "oh") == 50.0
     }
 
     def "getCurrentConditions should return current weather conditions of city"() {
         setup:
-           def slurper = [parseText: {
+            JsonSlurper.metaClass.parseText = {
                String json -> ["weather":["description": "sky is clear"]]
-           }] as JsonSlurper
-           Weather.slurper = slurper
+           }
            
         expect:
            Weather.getCurrentConditions("columbus", "oh") == "sky is clear"
@@ -29,10 +27,9 @@ class WeatherSpec extends Specification {
 
     def "getFiveDayForecast should return temperatures of next five days of city"() {
         setup:
-           def slurper = [parseText: {
+            JsonSlurper.metaClass.parseText = {
                String json -> ["list":[["temp":["day":10.0]], ["temp":["day":20.0]], ["temp":["day":30.0]], ["temp":["day":40.0]], ["temp":["day":50.0]], ["temp":["day":60.0]], ["temp":["day":70.0]]]]
-           }] as JsonSlurper
-           Weather.slurper = slurper
+           }
            
         expect:
            Weather.getFiveDayForecast("columbus", "oh") == [10.0, 20.0, 30.0, 40.0, 50.0]
